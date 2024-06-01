593dd76bfcf74662bf8a4b82a6481460dcca266b
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-01 11:41:57 +0700

    feat: add useState concept

--- src/components/UseStateComponent.jsx
+++ src/components/UseStateComponent.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStateComponent = () => {
+  const [count, setCount] = useState(0)
+
+  const handleIncrement = () => {
+    setCount(count => count + 1) //? Cara ini kita amil nilai statenya baru di tambah
+  }
+
+  const handleDecrement = () => {
+    setCount(count => count - 1)
+  }
+
+ 
+
   return (
     <div>
       <div className="hooks">useState</div>
       <div className="content">
-        <button>decrement</button>
-        <div>0</div>
-        <button>increment</button>
+        <button onClick={handleDecrement} >decrement</button>
+        <div>{count}</div>
+        <button onClick={handleIncrement}>increment</button>
       </div>
     </div>
   )
