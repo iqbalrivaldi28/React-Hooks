c0d98e2df7730424eaaa7b78d622657f1b9bb417
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-01 09:48:49 +0700

    ref: refactor folder and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import UseStateComponent from './components/UseStateComponent';
 
 function App() {
-  const [count, setCount] = useState(() => {
-    const savedCount = localStorage.getItem('count')
-    return savedCount !== null ? parseInt(savedCount) : 0
-  })
-
-
-  const handleClick = () => {
-    setCount((prevCount) => {
-      const newCount = prevCount + 1;
-      localStorage.setItem('count', newCount)
-      return newCount
-    })
-  }
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <UseStateComponent />
     </>
+
   )
 }
 
-export default App
+export default App;
---
+++ src/components/UseStateComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UseStateComponent = () => {
+  return (
+    <div>
+      <div className="hooks">useState</div>
+      <div className="content">
+        <button>decrement</button>
+        <div>0</div>
+        <button>increment</button>
+      </div>
+    </div>
+  )
+}
+
+export default UseStateComponent
